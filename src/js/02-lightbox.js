514217d7e6e093b2d790436c5a8ee1325fb42b2f
Valerii Arlamovskyi <valera.arl96@gmail.com>
2024-03-30 20:32:01 -0400

    save changes

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,30 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+        title="${description}"
+      />
+    </a>
+    </li>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", markup);
+
+const lightbox = new SimpleLightbox(".gallery__link", {
+    captions:true,
+    captionDelay:1000,
+  });
+
+
